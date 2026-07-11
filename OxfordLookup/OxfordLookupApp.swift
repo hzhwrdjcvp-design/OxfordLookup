941a1996aa0eab261fcbbaefc0680f84525883f1
Minghui Khan <805065976@qq.com>
2026-07-11 14:45:20 -0400

    Initial Commit

---
+++ OxfordLookup/OxfordLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  OxfordLookupApp.swift
+//  OxfordLookup
+//
+//  Created by Khan on 7/11/26.
+//
+
+import SwiftUI
+
+@main
+struct OxfordLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
